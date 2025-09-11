277c07e41e6ad27b3d8adf57d49e3712c10782a2
Kamran Miralami <kamran.m1380@yahoo.com>
2025-09-11 23:10:18 +0200

    feat(Initializations): added some initial folder and scripts

---
+++ Assets/_Scripts/Utilities/ScriptableObjectBehavior.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public abstract class ScriptableObjectBehavior : ScriptableObject
+{
+    public virtual void Initialize() { }
+    public virtual void Update() { }
+    public virtual void OnApplicationPause(bool pause) { }
+    public virtual void OnApplicationQuit() { }
+}
