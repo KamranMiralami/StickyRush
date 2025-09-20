611602b4d7639f28c648f6ce9c3fc0ae7a25c09c
MaxElano <114808761+MaxElano@users.noreply.github.com>
2025-09-21 00:29:10 +0200

    feat/ref: animations fully working
    Already added support for animations going left and right and added checks that these values can also be null, because we don't have these animations yet

--- Assets/_Scripts/SetDirectionalAnimations.cs
+++ Assets/_Scripts/SetDirectionalAnimations.cs
@@ -5,25 +5,43 @@ public class SetDirectionalAnimations : MonoBehaviour
     [Header("Animations")]
     [SerializeField] GameObject upGameObject;
     [SerializeField] GameObject downGameObject;
+    [SerializeField] GameObject leftGameObject;
+    [SerializeField] GameObject rightGameObject;
     
     public void SetDirection(Vector2 dir)
     {
         //Change animation according to direction 
-        if (dir == Vector2.up)
+        if (dir == Vector2.up && upGameObject)
         {
             TurnOffAllAnimations();
             upGameObject.SetActive(true);
         }
-        else if (dir == Vector2.down)
+        else if (dir == Vector2.down && downGameObject)
         {
             TurnOffAllAnimations();
             downGameObject.SetActive(true);
+        }
+        else if (dir == Vector2.left && leftGameObject)
+        {
+            TurnOffAllAnimations();
+            leftGameObject.SetActive(true);
+        }
+        else if (dir == Vector2.right && rightGameObject)
+        {
+            TurnOffAllAnimations();
+            rightGameObject.SetActive(true);
         }
     }
     
     private void TurnOffAllAnimations()
     {
-        upGameObject.SetActive(false);
-        downGameObject.SetActive(false);
+        if (upGameObject)
+            upGameObject.SetActive(false);
+        if (downGameObject)
+            downGameObject.SetActive(false);
+        if (leftGameObject)
+            leftGameObject.SetActive(false);
+        if (rightGameObject)
+            rightGameObject.SetActive(false);
     }
 }
